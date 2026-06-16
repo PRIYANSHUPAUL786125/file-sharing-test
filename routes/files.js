21df4afb513af193227338a9af7f9d490f211182
Priyanshu <tskpriyanshu2005@gmail.com>
2026-06-16 07:58:03 +0000

    server test

--- routes/files.js
+++ routes/files.js
@@ -4,4 +4,7 @@ const {uploadfile,showfile,sendFile}=require('../controllers/files.js');
 router.post('/',upload,uploadfile)
 router.get('/download/:uuid',showfile);
 router.post("/send",sendFile)
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
 module.exports=router
