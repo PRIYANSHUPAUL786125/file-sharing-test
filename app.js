65acf1d4931a45550afe9a1dfa35b800f3550898
Priyanshu <tskpriyanshu2005@gmail.com>
2026-06-20 06:20:25 +0000

    cors update

--- app.js
+++ app.js
@@ -11,8 +11,18 @@ const errorMiddleware=require('./middlewares/error.middleware.js');
 require("./utils/cron.js");
 app.use(express.urlencoded({ extended: true }));
 app.set("trust proxy", 1); // 👈 ADD THIS LINE HERE!
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://project-5tw7a.vercel.app",
+  "https://project-5tw7a-plydefleu-paul12345.vercel.app",
+];
+
 app.use(cors({
-    origin:'https://project-5tw7a-plydefleu-paul12345.vercel.app',
+  origin: allowedOrigins,
+  credentials: true,
+}));
+app.use(cors({
+    origin:allowedOrigins,
     credentials:true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
